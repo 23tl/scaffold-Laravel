67a4163cee977032054eeb27d6afbb5da7a6a1a4
Strays <784494731@qq.com>
2022-06-15 16:46:45 +0800

    更新 版本 更新模型基类

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,13 +3,6 @@
 
 namespace App\Http\Controllers;
 
-
-use App\Http\Requests\TestRequest;
-
 class TestController extends Controller
 {
-    public function index(TestRequest $request)
-    {
-        return 'Hello World';
-    }
 }
--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -5,8 +5,67 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+
+/**
+ * App\Models\BaseModel
+ *
+ *
+ * 属性声明
+ * @method Builder|Model orderByDefault(string $column = 'id', string $direction = 'desc')
+ *
+ * @mixin Model
+ */
 class BaseModel extends Model
 {
-    
+    use SoftDeletes;
+
+    /**
+     * @var string
+     */
+    protected $connection = 'mysql';
+
+    /**
+     * @var string[] 允许查询的字段
+     */
+    public $guarded = ['id', 'createdTime', 'updatedTime', 'deletedTime'];
+
+    /**
+     * @var string[] 转为数组需隐藏的字段
+     */
+    protected $hidden = ['id', 'userId'];
+
+    /**
+     * @var string
+     */
+    protected $dateFormat = 'U';
+
+    /**
+     *  @var string|null
+     */
+    public const CREATED_AT = 'createdTime';
+
+    /**
+     *  @var string|null
+     */
+    public const UPDATED_AT = 'updatedTime';
+
+    /**
+     * @var  string|null
+     */
+    public const DELETED_AT = 'deletedTime';
+
+    /**
+     * @param Builder $query
+     * @param string  $column
+     * @param string  $direction
+     *
+     * @return Builder
+     */
+    public function scopeOrderByDefault(Builder $query, string $column = 'id', string $direction = 'desc'): Builder
+    {
+        return $query->orderBy($column, $direction);
+    }
 }
